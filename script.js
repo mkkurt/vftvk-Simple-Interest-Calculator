88a556203c39e64c2555e52b2c0f1320a5706f9a
lmoonwolf <kutaykurt99@gmail.com>
2021-05-16 02:33:48 +0300

    improvements

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ function handleSubmit() {
   var amount = document.getElementById("amount").value;
   var rate = document.getElementById("rate").value;
   var years = document.getElementById("years").value;
+  var result = document.getElementById("result");
+
   var profit = amount * (rate * 0.01) * years;
-  var payment = (parseInt(amount) + parseInt(profit)).toFixed(2);
-  var result = document.getElementById("results");
-  result.innerHTML = `What you receive: ${payment}`;
+  var total = parseInt(amount) + parseInt(profit);
+
+  if (years != 1) {
+    var yearPlural = "years";
+  } else {
+    yearPlural = "year";
+  }
+
+  if (amount == "") {
+    result.style.display = "block";
+    result.innerHTML = "Please fill out the required inputs";
+  } else {
+    result.style.display = "block";
+    result.innerHTML = `Your ${amount}$ will become ${total}$ after ${years} ${yearPlural}`;
+  }
 }
+
 function handleRange() {
   var rateText = document.getElementById("rateText");
   var input = document.getElementById("rate").value;
