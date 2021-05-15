136caa1bf1955e7f659d9f67b80e7e5888b987ae
lmoonwolf <kutaykurt99@gmail.com>
2021-05-15 22:17:38 +0300

    working now

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function handleSubmit() {
+  var amount = document.getElementById("amount").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var profit = amount * (rate * 0.01) * years;
+  var payment = (parseInt(amount) + parseInt(profit)).toFixed(2);
+  var result = document.getElementById("results");
+  result.innerHTML = `What you receive: ${payment}`;
 }
-        
+function handleRange() {
+  var rateText = document.getElementById("rateText");
+  var input = document.getElementById("rate").value;
+  rateText.innerHTML = input + "%";
+}
